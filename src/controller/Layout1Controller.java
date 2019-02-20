b598ebe27b06a22119b341598e77735ff882b431
Henrique Rosa <henrique.prosa@hotmail.com>
2019-02-20 16:43:26 -0300

    Added combo box do show connected devices

--- src/controller/Layout1Controller.java
+++ src/controller/Layout1Controller.java
@@ -7,13 +7,13 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class Layout1Controller {
-
 
     @FXML
     private ResourceBundle resources;
@@ -34,14 +34,17 @@ public class Layout1Controller {
     private TextArea txaLog;
 
     @FXML
+    private ComboBox<?> cbxDevices;
+
+    @FXML
     void initialize() {
         assert txtFieldFileAddress != null : "fx:id=\"txtFieldFileAddress\" was not injected: check your FXML file 'Layout1.fxml'.";
         assert progressBar != null : "fx:id=\"progressBar\" was not injected: check your FXML file 'Layout1.fxml'.";
         assert btnDeploy != null : "fx:id=\"btnDeploy\" was not injected: check your FXML file 'Layout1.fxml'.";
         assert txaLog != null : "fx:id=\"txaLog\" was not injected: check your FXML file 'Layout1.fxml'.";
+        assert cbxDevices != null : "fx:id=\"cbxDevices\" was not injected: check your FXML file 'Layout1.fxml'.";
 
     }
-    
     @FXML
     void selectFile(MouseEvent event) {
     	
