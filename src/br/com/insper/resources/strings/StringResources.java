e831d85996001abdfa355a1293f289b5dfcb719b
henrique.prosa@hotmail.com <henrique.prosa@hotmail.com>
2019-11-19 14:43:30 -0300

    Missing final in const vars

--- src/br/com/insper/resources/strings/StringResources.java
+++ src/br/com/insper/resources/strings/StringResources.java
@@ -8,48 +8,48 @@ public class StringResources {
 
 	//MultiDeployer
 	public static final String INSPER = "https://www.insper.edu.br";
-	public static String LAYOUT1 = "br/com/insper/view/Layout1.fxml";
-	public static String LAYOUT2 = "br/com/insper/view/Layout2.fxml";
-	public static String LAYOUT3 = "br/com/insper/view/Layout3.fxml";
-	public static String LOGO = "/br/com/insper/resources/img/logo.png";
-	public static String UPPERBAR_TITLE = " Multi Deployer Of Apps";
+	public static final String LAYOUT1 = "br/com/insper/view/Layout1.fxml";
+	public static final String LAYOUT2 = "br/com/insper/view/Layout2.fxml";
+	public static final String LAYOUT3 = "br/com/insper/view/Layout3.fxml";
+	public static final String LOGO = "/br/com/insper/resources/img/logo.png";
+	public static final String UPPERBAR_TITLE = " Multi Deployer Of Apps";
 
 	//Layout1Controller
-	public static String LB_OS = "fx:id=\"lblOS\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String TXT_FIELD_APP_PATH = "fx:id=\"txtFieldAppPath\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_APP_FILE = "fx:id=\"btnAppFile\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String PROGRESS_BAR = "fx:id=\"progressBar\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String TXT_FIELD_ADB_PATH = "fx:id=\"txtFieldAdbPath\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_ADB_FILE = "fx:id=\"btnAdbFile\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String CB_DEVICES = "fx:id=\"cbDevices\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_INSTALL = "fx:id=\"btnInstall\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_LAUNCH = "fx:id=\"btnLaunch\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String TXA_LOG = "fx:id=\"txaLog\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_UNINSTALL = "fx:id=\"btnUninstall\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String BTN_CLEAR = "fx:id=\"btnClear\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String MI_CLOSE = "fx:id=\"miClose\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String MI_CLEAR_LOGS = "fx:id=\"miClearLogs\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String MI_ABOUT_US = "fx:id=\"miAboutUs\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String MI_BUG_REPORT = "fx:id=\"miBugReport\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String MI_GIT_CODE = "fx:id=\"miGitCode\" was not injected: check your FXML file 'Layout1.fxml'.";
-	public static String IMG_INSPER = "fx:id=\"imgInsper\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String LB_OS = "fx:id=\"lblOS\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String TXT_FIELD_APP_PATH = "fx:id=\"txtFieldAppPath\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_APP_FILE = "fx:id=\"btnAppFile\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String PROGRESS_BAR = "fx:id=\"progressBar\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String TXT_FIELD_ADB_PATH = "fx:id=\"txtFieldAdbPath\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_ADB_FILE = "fx:id=\"btnAdbFile\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String CB_DEVICES = "fx:id=\"cbDevices\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_INSTALL = "fx:id=\"btnInstall\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_LAUNCH = "fx:id=\"btnLaunch\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String TXA_LOG = "fx:id=\"txaLog\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_UNINSTALL = "fx:id=\"btnUninstall\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String BTN_CLEAR = "fx:id=\"btnClear\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String MI_CLOSE = "fx:id=\"miClose\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String MI_CLEAR_LOGS = "fx:id=\"miClearLogs\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String MI_ABOUT_US = "fx:id=\"miAboutUs\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String MI_BUG_REPORT = "fx:id=\"miBugReport\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String MI_GIT_CODE = "fx:id=\"miGitCode\" was not injected: check your FXML file 'Layout1.fxml'.";
+	public static final String IMG_INSPER = "fx:id=\"imgInsper\" was not injected: check your FXML file 'Layout1.fxml'.";
 
-	public static String WINDOWS_DETECTED = "# Windows Detected\n\n";
-	public static String LINUX_DETECTED = "# Linux Detected\n\n";
-	public static String MAC_DETECTED = "# Mac OS Detected\n\n";
+	public static final String WINDOWS_DETECTED = "# Windows Detected\n\n";
+	public static final String LINUX_DETECTED = "# Linux Detected\n\n";
+	public static final String MAC_DETECTED = "# Mac OS Detected\n\n";
 	
 	
-	public static String EXAMPLE_APK_PATH_HEADER = "- Example of .apk path:\n";
-	public static String EXAMPLE_APK_PATH_WINDOWS = "C:\\..\\MyApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk\n\n";
-	public static String EXAMPLE_ADB_PATH_HEADER = "- Example of ADB path:\n";
-	public static String EXAMPLE_ADB_PATH_WINDOWS = "C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb.exe\n\n";
+	public static final String EXAMPLE_APK_PATH_HEADER = "- Example of .apk path:\n";
+	public static final String EXAMPLE_APK_PATH_WINDOWS = "C:\\..\\MyApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk\n\n";
+	public static final String EXAMPLE_ADB_PATH_HEADER = "- Example of ADB path:\n";
+	public static final String EXAMPLE_ADB_PATH_WINDOWS = "C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb.exe\n\n";
 
 
-	public static String EXAMPLE_APK_PATH_LINUX = "/home/user/AndroidStudioProjects/app/build/app/outputs/apk/app.apk\n\n";
-	public static String EXAMPLE_ADB_PATH_LINUX = "/home/user/Android/Sdk/platform-tools/adb\n\n";
+	public static final String EXAMPLE_APK_PATH_LINUX = "/home/user/AndroidStudioProjects/app/build/app/outputs/apk/app.apk\n\n";
+	public static final String EXAMPLE_ADB_PATH_LINUX = "/home/user/Android/Sdk/platform-tools/adb\n\n";
 	
-	public static String EXAMPLE_APK_PATH_MAC = "C:\\..\\MyApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk\n\n";
-	public static String EXAMPLE_ADB_PATH_MAC = "/Users/agile/Library/Android/sdk/platform-tools/adb\n\n";
+	public static final String EXAMPLE_APK_PATH_MAC = "C:\\..\\MyApp\\app\\build\\outputs\\apk\\debug\\app-debug.apk\n\n";
+	public static final String EXAMPLE_ADB_PATH_MAC = "/Users/agile/Library/Android/sdk/platform-tools/adb\n\n";
 
 
 	public static final String DEVICES_IS_NULL = "Devices list is NULL.";
@@ -65,13 +65,13 @@ public class StringResources {
 
 
 	//Layout2Controller
-	public static String BTN_CLOSE_LAYOUT2 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout2.fxml'.";
-	public static String HLINK_HASHIMOTO = "fx:id=\"hlinkHashimoto\" was not injected: check your FXML file 'Layout2.fxml'.";
-	public static String HLINK_HENRIQUEROSA = "fx:id=\"hlinkHenriqueRosa\" was not injected: check your FXML file 'Layout2.fxml'.";
+	public static final String BTN_CLOSE_LAYOUT2 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout2.fxml'.";
+	public static final String HLINK_HASHIMOTO = "fx:id=\"hlinkHashimoto\" was not injected: check your FXML file 'Layout2.fxml'.";
+	public static final String HLINK_HENRIQUEROSA = "fx:id=\"hlinkHenriqueRosa\" was not injected: check your FXML file 'Layout2.fxml'.";
 
 	//Layout3Controller
-	public static String BTN_CLOSE_LAYOUT3 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout3.fxml'.";
-	public static String BTN_COPY_LAYOUT3 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout3.fxml'.";
-	public static String HLINK_ISSUE = "fx:id=\"hlinkIssue\" was not injected: check your FXML file 'Layout3.fxml'.";
-	public static String LBL_ADVICE = "fx:id=\"lblAdvice\" was not injected: check your FXML file 'Layout3.fxml'.";
+	public static final String BTN_CLOSE_LAYOUT3 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout3.fxml'.";
+	public static final String BTN_COPY_LAYOUT3 = "fx:id=\"btnClose\" was not injected: check your FXML file 'Layout3.fxml'.";
+	public static final String HLINK_ISSUE = "fx:id=\"hlinkIssue\" was not injected: check your FXML file 'Layout3.fxml'.";
+	public static final String LBL_ADVICE = "fx:id=\"lblAdvice\" was not injected: check your FXML file 'Layout3.fxml'.";
 }
